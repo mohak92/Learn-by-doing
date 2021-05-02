65eea54eb9ccda0c1a34aa4023bae975f342bee5
mohak92 <tamhane.mohak@gmail.com>
2021-05-02 03:05:53 -0400

    Initial Commit

---
+++ Learn-by-doingTests/Learn_by_doingTests.swift
@@ -0,0 +1,33 @@
+//
+//  Learn_by_doingTests.swift
+//  Learn-by-doingTests
+//
+//  Created by Mohak Tamhane on 5/2/21.
+//
+
+import XCTest
+@testable import Learn_by_doing
+
+class Learn_by_doingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
